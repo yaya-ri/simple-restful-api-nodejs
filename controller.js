8ed2f8abdd1707ac2aea13d25319a9e5fa03222d
yaya <yaya.rizqi.isnanda@gmail.com>
2018-12-06 17:28:08 +0700

    simple CRUD

--- controller.js
+++ controller.js
@@ -28,6 +28,53 @@ exports.findUser = function(req, res){
     });
 };
 
+exports.createUser = function(req,res){
+    var first_name = req.body.first_name;
+    var last_name = req.body.last_name;
+
+    connection.query('INSERT INTO person (first_name, last_name) values (?,?)',
+    [first_name,last_name],
+    function (error, rows, fields){
+        if(error){
+            console.log(error);
+        }else{
+            response.ok("Success!!",res);
+        }
+    });
+};
+
+exports.updateUser = function(req, res) {
+    
+    var user_id = req.params.user_id;
+    var first_name = req.body.first_name;
+    var last_name = req.body.last_name;
+
+    connection.query('UPDATE person SET first_name = ?, last_name = ? WHERE id = ?',
+    [ first_name, last_name, user_id ], 
+    function (error, rows, fields){
+        if(error){
+            console.log(error)
+        } else{
+            response.ok("Success!!", res)
+        }
+    });
+};
+
+exports.deleteUser = function(req, res) {
+    
+    var user_id = req.params.user_id;
+
+    connection.query('DELETE FROM person WHERE id = ?',
+    [ user_id ], 
+    function (error, rows, fields){
+        if(error){
+            console.log(error)
+        } else{
+            response.ok("Success!!", res)
+        }
+    });
+};
+
 
 
 exports.index = function(req,res){
--- routes.js
+++ routes.js
@@ -6,4 +6,7 @@ module.exports =  function(app){
     app.route('/').get(todoList.index);
     app.route('/users').get(todoList.users);
     app.route('/user/:user_id').get(todoList.findUser);
+    app.route('/user/insert').post(todoList.createUser);
+    app.route('/user/:user_id').put(todoList.updateUser);
+    app.route('/user/:user_id').delete(todoList.deleteUser);
 };
